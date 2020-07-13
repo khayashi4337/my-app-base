c0ed09ef5c5cb1c322659963cd1f6b7c06f8acc8
hayashi <khayashi4337@gmail.com>
2020-07-13 18:29:00 +0900

    コメントを書きやすいように修正

--- src/main/webapp/app/modules/page02/page02.tsx
+++ src/main/webapp/app/modules/page02/page02.tsx
@@ -17,11 +17,8 @@ export const Page02 = (props: IPage02Prop) => {
       <Sidemenu/>
       <Col md="7">
         <h2>
-          <Translate contentKey="page02.title">Page02タイトル</Translate>
+          予約内容選択
         </h2>
-        <p className="lead">
-          <Translate contentKey="page02.subtitle">Page02サブタイトル</Translate>
-        </p>
         {account && account.login ? (
           <div>
             <Alert color="success">
@@ -32,28 +29,26 @@ export const Page02 = (props: IPage02Prop) => {
           </div>
         ) : (
           <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
 
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
           </div>
         )}
-
-
+        <p>| メニュー＿＿必須＿＿＿＿＿＿＿＿＿＿＿＿＿＿変更</p>
+        <p>|＿未選択＿＿＿＿メニューを選択してください。</p>
+        <p>|</p>
+        <p></p>
+        <p>｜＿スタッフ＿必須＿＿＿＿＿＿＿＿＿＿＿＿＿＿変更</p>
+        <p>｜＿＿指名しない</p>
+        <p>｜＿＿ご希望のメニュー、日時で対応可能なスタッフが対応します。</p>
+        <p>｜＿＿スタッフが対応します。</p>
+        <p>｜</p>
+        <p></p>
+        <p>｜＿日時＿＿必須＿＿＿＿＿＿変更</p>
+        <p>｜＿未選択＿＿＿日時を選択してください。</p>
+        <p></p>
+        <p></p>
+        <p>確定して確認画面へ進む</p>
+        <p>｜</p>
+        <p></p>
       </Col>
       {/* 右側のイラスト */}
       <Col md="3" className="pad">
--- src/main/webapp/app/modules/pages02/page20/page20.tsx
+++ src/main/webapp/app/modules/pages02/page20/page20.tsx
@@ -32,27 +32,27 @@ export const Page20 = (props: IPage20Prop) => {
             </Alert>
           </div>
         ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
+          <div></div>
         )}
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
 
 
       </Col>
