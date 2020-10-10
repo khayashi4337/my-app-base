e7ed2484ba88694fdebeb4d64a9c9843a55a838e
lapimpact2020 <wildhyeonjin@gmail.com>
2020-10-10 19:34:52 +0900

    add calendar

--- src/main/webapp/app/modules/pages01/page15/page15.tsx
+++ src/main/webapp/app/modules/pages01/page15/page15.tsx
@@ -7,8 +7,91 @@ import { connect } from 'react-redux';
 import { Row, Col, Alert } from 'reactstrap';
 
 import { Sidemenu } from '../../../shared/sidemenu/Sidemenu'
+import styled from 'styled-components';
+import { DefaultButton, initializeIcons } from '@fluentui/react';
+import { Checkbox } from '@fluentui/react';
+
+import { DateRangeType } from 'office-ui-fabric-react/lib/Calendar';
+import { CalendarInlineExample } from '../../calendar/calendar'
+
+
+function _onChange(ev: React.FormEvent<HTMLElement>, isChecked: boolean) {
+  // eslint-disable-next-line no-console
+  console.log(`The option has been changed to ${isChecked}.`);
+}
 
 export type IPage15Prop = StateProps;
+
+class MyCalendarArgs {
+  title: string;
+
+  constructor(
+    title: string,
+    ) {
+    this.title = title;
+  }
+}
+
+const MyCalendar = (args: MyCalendarArgs) => {
+  return <React.Fragment>
+
+  <h4>{args.title}</h4>
+  <Row>
+    <Col md="1">日</Col>
+    <Col md="1">月</Col>
+    <Col md="1">火</Col>
+    <Col md="1">水</Col>
+    <Col md="1">木</Col>
+    <Col md="1">金</Col>
+    <Col md="1">土</Col>
+  </Row>
+  <Row>
+    <Col md="1"></Col>
+    <Col md="1"></Col>
+    <Col md="1"></Col>
+    <Col md="1">01</Col>
+    <Col md="1">02</Col>
+    <Col md="1">03</Col>
+    <Col md="1">04</Col>
+  </Row>
+  <Row>
+    <Col md="1">05</Col>
+    <Col md="1">06</Col>
+    <Col md="1">07</Col>
+    <Col md="1">08</Col>
+    <Col md="1">09</Col>
+    <Col md="1">10</Col>
+    <Col md="1">11</Col>
+  </Row>
+  <Row>
+    <Col md="1">12</Col>
+    <Col md="1">13</Col>
+    <Col md="1">14</Col>
+    <Col md="1">15</Col>
+    <Col md="1">16</Col>
+    <Col md="1">17</Col>
+    <Col md="1">18</Col>
+  </Row>
+  <Row>
+    <Col md="1">19</Col>
+    <Col md="1">20</Col>
+    <Col md="1">21</Col>
+    <Col md="1">22</Col>
+    <Col md="1">23</Col>
+    <Col md="1">24</Col>
+    <Col md="1">25</Col>
+  </Row>
+  <Row>
+    <Col md="1">26</Col>
+    <Col md="1">27</Col>
+    <Col md="1">28</Col>
+    <Col md="1">29</Col>
+    <Col md="1">30</Col>
+    <Col md="1">31</Col>
+    <Col md="1"></Col>
+  </Row>
+  </React.Fragment>;
+}
 
 {/* i18n: src/main/webapp/i18n */}
 export const Page15 = (props: IPage15Prop) => {
@@ -35,24 +118,44 @@ export const Page15 = (props: IPage15Prop) => {
           <div></div>
         )}
 
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
+        <div className="radius1">
+        <CalendarInlineExample isMonthPickerVisible={ false } dateRangeType={ DateRangeType.Day } autoNavigateOnSelection={ false } showGoToToday={ false } />
+        </div>
+
+        <div className="radius1">
+          <MyCalendar title="2020年7月"/>
+        </div>
+
+        <div className="radius1">
+          <MyCalendar title="2020年8月"/>
+        </div>
+
+        <Row>
+          <Col md="1">
+            <Checkbox label="当日" onChange={_onChange} />
+          </Col>
+          <Col>
+            <Checkbox label="選択した日付・時間" onChange={_onChange} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md="1"><div className="radius3">15:00</div></Col>
+          <Col md="1"><div className="radius3">15:30</div></Col>
+          <Col md="1"><div className="radius3">16:00</div></Col>
+          <Col md="1"><div className="radius3">16:30</div></Col>
+        </Row>
+        <Row>
+          <Col md="1"><div className="radius3">17:00</div></Col>
+          <Col md="1"><div className="radius3">17:30</div></Col>
+          <Col md="1"><div className="radius3">18:00</div></Col>
+          <Col md="1"><div className="radius3">18:30</div></Col>
+        </Row>
+        <Row>
+          <Col md="1"><div className="radius3">19:00</div></Col>
+          <Col md="1"><div className="radius3">19:30</div></Col>
+          <Col md="1"><div className="radius3">20:00</div></Col>
+          <Col md="1"><div className="radius3">20:30</div></Col>
+        </Row>
 
       </Col>
       {/* 右側のイラスト */}
@@ -71,3 +174,11 @@ const mapStateToProps = storeState => ({
 type StateProps = ReturnType<typeof mapStateToProps>;
 
 export default connect(mapStateToProps)(Page15);
+
+const RedButton = styled(DefaultButton)`
+  background-color: #FF0000;
+  color: #ffffff;
+  vertical-align: bottom;
+  margin-top: 10px;
+  margin-left: 10px;
+`;
