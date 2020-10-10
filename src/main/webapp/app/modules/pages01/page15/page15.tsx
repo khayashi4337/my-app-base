80e74bceef2596c3e07df7e188360280d10d5d18
hayashi <khayashi4337@gmail.com>
2020-10-10 19:18:48 +0900

    microsoftのfaclicにさしかえてみた。

--- src/main/webapp/app/modules/pages01/page15/page15.tsx
+++ src/main/webapp/app/modules/pages01/page15/page15.tsx
@@ -11,12 +11,200 @@ import styled from 'styled-components';
 import { DefaultButton, initializeIcons } from '@fluentui/react';
 import { Checkbox } from '@fluentui/react';
 
+import { Calendar, DayOfWeek, DateRangeType } from 'office-ui-fabric-react/lib/Calendar';
+import { addDays, getDateRangeArray } from '@fluentui/date-time-utilities';
+
 function _onChange(ev: React.FormEvent<HTMLElement>, isChecked: boolean) {
   // eslint-disable-next-line no-console
   console.log(`The option has been changed to ${isChecked}.`);
 }
 
 export type IPage15Prop = StateProps;
+
+export interface ICalendarInlineExampleProps {
+  isMonthPickerVisible?: boolean;
+  dateRangeType: DateRangeType;
+  autoNavigateOnSelection: boolean;
+  showGoToToday: boolean;
+  showNavigateButtons?: boolean;
+  highlightCurrentMonth?: boolean;
+  highlightSelectedMonth?: boolean;
+  isDayPickerVisible?: boolean;
+  showMonthPickerAsOverlay?: boolean;
+  showWeekNumbers?: boolean;
+  minDate?: Date;
+  maxDate?: Date;
+  restrictedDates?: Date[];
+  showSixWeeksByDefault?: boolean;
+  workWeekDays?: DayOfWeek[];
+  firstDayOfWeek?: DayOfWeek;
+}
+
+const dayPickerStrings = {
+  months: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  shortDays: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  goToToday: 'Go to today',
+  weekNumberFormatString: 'Week number {0}',
+  prevMonthAriaLabel: 'Previous month',
+  nextMonthAriaLabel: 'Next month',
+  prevYearAriaLabel: 'Previous year',
+  nextYearAriaLabel: 'Next year',
+  prevYearRangeAriaLabel: 'Previous year range',
+  nextYearRangeAriaLabel: 'Next year range',
+  closeButtonAriaLabel: 'Close',
+  monthPickerHeaderAriaLabel: '{0}, select to change the year',
+  yearPickerHeaderAriaLabel: '{0}, select to change the month',
+};
+const divStyle: React.CSSProperties = {
+  height: 'auto',
+};
+const buttonStyle: React.CSSProperties = {
+  margin: '17px 10px 0 0',
+};
+let dateRangeString: string | null = null;
+
+export const CalendarInlineExample: React.FunctionComponent<MyCalendarArgs> = (
+  args: MyCalendarArgs,
+) => {
+
+  const props: ICalendarInlineExampleProps = {
+    dateRangeType: DateRangeType.Month,
+    autoNavigateOnSelection: false,
+    showGoToToday: true
+  };
+
+  const [selectedDateRange, setSelectedDateRange] = React.useState<Date[]>();
+  const [selectedDate, setSelectedDate] = React.useState<Date>();
+
+  const onSelectDate = (date: Date, dateRangeArray: Date[]): void => {
+    setSelectedDate(date);
+    setSelectedDateRange(dateRangeArray);
+  };
+
+  const goPrevious = () => {
+    const goPreviousSelectedDate = selectedDate || new Date();
+    const dateRangeArray = getDateRangeArray(goPreviousSelectedDate, props.dateRangeType, DayOfWeek.Sunday);
+    let subtractFrom = dateRangeArray[0];
+    let daysToSubtract = dateRangeArray.length;
+    if (props.dateRangeType === DateRangeType.Month) {
+      subtractFrom = new Date(subtractFrom.getFullYear(), subtractFrom.getMonth(), 1);
+      daysToSubtract = 1;
+    }
+    const newSelectedDate = addDays(subtractFrom, -daysToSubtract);
+    return {
+      goPreviousSelectedDate: newSelectedDate,
+    };
+  };
+
+  const goNext = () => {
+    const goNextSelectedDate = selectedDate || new Date();
+    const dateRangeArray = getDateRangeArray(goNextSelectedDate, props.dateRangeType, DayOfWeek.Sunday);
+    const newSelectedDate = addDays(dateRangeArray ? dateRangeArray.pop() : null, 1);
+
+    return {
+      goNextSelectedDate: newSelectedDate,
+    };
+  };
+
+  const onDismiss = () => {
+    return selectedDate;
+  };
+
+  if (selectedDateRange) {
+    const rangeStart = selectedDateRange[0];
+    const rangeEnd = selectedDateRange[selectedDateRange.length - 1];
+    dateRangeString = rangeStart.toLocaleDateString() + '-' + rangeEnd.toLocaleDateString();
+  }
+
+  return (
+    <div style={divStyle}>
+      {
+        <div>
+          Selected date(s): <span>{!selectedDate ? 'Not set' : selectedDate?.toLocaleString()}</span>
+        </div>
+      }
+      <div>
+        Selected dates:
+        <span> {!dateRangeString ? 'Not set' : dateRangeString}</span>
+      </div>
+      {(props.minDate || props.maxDate) && (
+        <div>
+          Date boundary:
+          <span>
+            {' '}
+            {props.minDate ? props.minDate.toLocaleDateString() : 'Not set'}-
+            {props.maxDate ? props.maxDate.toLocaleDateString() : 'Not set'}
+          </span>
+        </div>
+      )}
+      {props.restrictedDates && (
+        <div>
+          Disabled date(s):
+          <span>
+            {' '}
+            {props.restrictedDates.length > 0
+              ? props.restrictedDates.map(d => d.toLocaleDateString()).join(', ')
+              : 'Not set'}
+          </span>
+        </div>
+      )}
+      <Calendar
+        // eslint-disable-next-line react/jsx-no-bind
+        onSelectDate={onSelectDate}
+        // eslint-disable-next-line react/jsx-no-bind
+        onDismiss={onDismiss}
+        isMonthPickerVisible={props.isMonthPickerVisible}
+        dateRangeType={props.dateRangeType}
+        autoNavigateOnSelection={props.autoNavigateOnSelection}
+        showGoToToday={props.showGoToToday}
+        value={selectedDate ? selectedDate : null}
+        firstDayOfWeek={props.firstDayOfWeek ? props.firstDayOfWeek : DayOfWeek.Sunday}
+        strings={dayPickerStrings}
+        highlightCurrentMonth={props.highlightCurrentMonth}
+        highlightSelectedMonth={props.highlightSelectedMonth}
+        isDayPickerVisible={props.isDayPickerVisible}
+        showMonthPickerAsOverlay={props.showMonthPickerAsOverlay}
+        showWeekNumbers={props.showWeekNumbers}
+        minDate={props.minDate}
+        maxDate={props.maxDate}
+        restrictedDates={props.restrictedDates}
+        showSixWeeksByDefault={props.showSixWeeksByDefault}
+        workWeekDays={props.workWeekDays}
+      />
+      {props.showNavigateButtons && (
+        <div>
+          <DefaultButton
+            style={buttonStyle}
+            // eslint-disable-next-line react/jsx-no-bind
+            onClick={goPrevious}
+            text="Previous"
+          />
+          <DefaultButton
+            style={buttonStyle}
+            // eslint-disable-next-line react/jsx-no-bind
+            onClick={goNext}
+            text="Next"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 class MyCalendarArgs {
   title: string;
@@ -27,6 +215,8 @@ class MyCalendarArgs {
 }
 
 const MyCalendar = (args: MyCalendarArgs) => {
+
+
   return <React.Fragment>
 
   <h4>{args.title}</h4>
@@ -90,6 +280,9 @@ const MyCalendar = (args: MyCalendarArgs) => {
 {/* i18n: src/main/webapp/i18n */}
 export const Page15 = (props: IPage15Prop) => {
   const { account } = props;
+
+  const dateRangeType: DateRangeType = DateRangeType.Month;
+
   return (
     <Row>
       <Sidemenu/>
@@ -114,7 +307,7 @@ export const Page15 = (props: IPage15Prop) => {
 
 
         <div className="radius1">
-          <MyCalendar title="2020年7月"/>
+          <CalendarInlineExample  title="2020年7月" />
         </div>
 
         <div className="radius1">
