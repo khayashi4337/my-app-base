f75bbd434d601f63cb056444878861f7e56293f3
hayashi <khayashi4337@gmail.com>
2020-11-15 23:57:05 +0900

    line sdk を使ってメッセージを送れるようになった。

---
+++ src/main/java/jp/boy/java/botclient/LineChannel.java
@@ -0,0 +1,27 @@
+﻿package jp.boy.java.botclient;
+
+import com.linecorp.bot.client.LineMessagingClient;
+
+/**
+ * ラインのチャンネル
+ * @author kh
+ *
+ */
+public class LineChannel {
+
+	private final String channelToken;
+	private final LineMessagingClient client;
+	
+	public LineChannel(String channelToken) {
+		this.channelToken = channelToken;
+		this.client = LineMessagingClient.builder(channelToken).build();
+	}
+	
+	public String getChannelToken() {
+		return channelToken;
+	}
+	
+	public LineMessagingClient getLineMessagingClient() {
+		return client;
+	}
+}
---
+++ src/main/java/jp/boy/java/domain/Staff.java
@@ -0,0 +1,6 @@
+﻿package jp.boy.java.domain;
+
+public class Staff {
+	public String name;
+	public String lineUserId;
+}
